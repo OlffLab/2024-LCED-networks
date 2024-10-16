5fb97b9878b91874ee451994169f3978253c0272
Han Olff <h.olff@rug.nl>
2024-10-16 15:44:24 +0200

    added a comment

--- rscripts/04 saltmarsh depressions.R
+++ rscripts/04 saltmarsh depressions.R
@@ -1,4 +1,5 @@
 #### analysis of the saltmarsh data we collected on monday with the LCED course
+# this is the script of yourname for the LCES course on schier
 
 # clear everything in the R environment
 rm(list = ls())
